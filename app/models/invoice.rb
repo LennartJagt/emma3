07909c762dc90106e554d35a1b71fe06bbcc9c9d
LennartJagt <lvdjagt@gmail.com>
2019-01-05 20:29:03 +0100

    aantal actieve clienten zichtbaar

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,6 +5,8 @@ class Invoice
   field :number, type: String
   field :amount, type: BigDecimal
   field :vat, type: BigDecimal
+  field "ref@Client@invoice", as: :client_id
   has_many :invoicedetails, foreign_key: "ref@Invoice@invoicedetail", primary_key: "_id"
+  belongs_to :client, foreign_key:"ref@Client@invoice" , primary_key: "_id"
   accepts_nested_attributes_for :invoicedetails, allow_destroy: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :clients
   resources :invoicedetails
   resources :invoices
   root "invoices#index"
